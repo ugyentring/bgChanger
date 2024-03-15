4983302ba9aaec40b5b68f431bd74c6cda76fb82
ugyentring <12220096.gcit@rub.edu.bt>
2024-03-15 16:45:09 +0600

    done with the bg Changer project using useState hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,38 @@
+import { useState } from "react";
+
 const App = () => {
+  const [color, setColor] = useState("white");
+
   return (
-    <div>
-      <h1 className="text-3xl font-bold underline">Hello, Ugyen</h1>
+    <div
+      className="w-full h-screen duration-200"
+      style={{ backgroundColor: color }}
+    >
+      <div className="fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2">
+        <div className="flex flex-wrap justify-center gap-3 shadow-lg bg-white px-3 py-2 rounded-3xl">
+          <button
+            onClick={() => setColor("green")}
+            className="outlin-none px-4 py-1 rounded-full text-white shadow-lg"
+            style={{ backgroundColor: "green" }}
+          >
+            Green
+          </button>
+          <button
+            onClick={() => setColor("red")}
+            className="outlin-none px-4 py-1 rounded-full text-white shadow-lg"
+            style={{ backgroundColor: "red" }}
+          >
+            Red
+          </button>
+          <button
+            onClick={() => setColor("blue")}
+            className="outlin-none px-4 py-1 rounded-full text-white shadow-lg"
+            style={{ backgroundColor: "blue" }}
+          >
+            Blue
+          </button>
+        </div>
+      </div>
     </div>
   );
 };
